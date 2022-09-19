19c242c9f3a1363604dfc93fbe22aec7a0628895
zxc <denis.bugai11@gmail.com>
2022-09-20 01:59:40 +0300

    Создал класс юзер и базу данных для сбора апи ключей.

--- binance_script.py
+++ binance_script.py
@@ -2,6 +2,27 @@ from binance.spot import Spot #as Client
 
 
 client = Spot()
+r = open('text.txt', 'w')
+class User():
+
+
+    def __init__(self, id):
+        self.id = id
+
+
+    def init_api(self, key_api, secret_api):
+        self.private_client = Spot(key=key_api, secret=secret_api)
+        data = {'User_id':self.id, 'Key':key_api, 'Secret':secret_api}
+        r = open('Data_base.txt', 'r')
+        for id in r['User_id']:
+            if id == self.id:
+                r.close()
+                break
+        r.close()
+        r = open('Data_base.txt', 'a')
+        r.write(str(data))
+        r.close()
+
 
 
 class Start_interface():
