731170a546eef34e14085ae5f0076e194cfe0aba
zxc <denis.bugai11@gmail.com>
2022-09-20 22:57:17 +0300

    Не могу скачать пулл из-за mainpy

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ import telebot
 from telebot import types
 import binance_script
 
+
+class current_monet():
+
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
 bot=telebot.TeleBot("1597496144:AAGukHlg_wigl2YA7-JYk1Ys_0Ai6Dx8Eq8")
 btn1 = types.KeyboardButton('Новости')
 btn2 = types.KeyboardButton('Крут баланса')
 btn3 = types.KeyboardButton('Помощь')
 btn4 = types.KeyboardButton('Стакан объём')
 btn5 = types.KeyboardButton('Показать весь объем')
+btn_none = types.KeyboardButton('По умолчанию')
+
 
 user=0
+
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
@@ -45,7 +60,7 @@ def cup(message):
     bot.register_next_step_handler(message, out)
 
 def out(message):
-    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
     markup1.add(btn1, btn2, btn3, btn4, btn5)
     try:
         token=binance_script.Start_interface(message.chat.id,message.text)
@@ -59,31 +74,57 @@ def volume(message):
     bot.send_message(message.chat.id, "Введите название монеты", parse_mode='html')
     bot.register_next_step_handler(message, volume_2)
 
-def volume_2(message):
-    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    markup1.add(btn1, btn2, btn3, btn4, btn5)
-    photo = open('graph.png', 'rb')
-    bot.send_photo(message.chat.id, photo=photo)
-    try:
 
-        token=binance_script.Start_interface(message.chat.id,message.text)
-        info=token.get_volume(2, 200)
+def volume_default(message):
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+    markup1.add(btn1, btn2, btn3, btn4, btn5)
+    global monet
+    print(monet.name)
+    try:
+        token=binance_script.Start_interface(message.chat.id,monet.name)
+        info=token.get_volume(2, message.text)
         bot.send_message(message.chat.id, f'Объём продаж: {info[0]}\nОбъём покупок: {info[1]}\n',parse_mode='html',reply_markup=markup1)
     except BaseException:
-        bot.send_message(message.chat.id, "Вы ввели неправильное название монеты")
+        bot.send_message(message.chat.id, "Вы ввели неправильное название монеты",reply_markup=markup1)
 
+def volume_2(message):
+    markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+    markup2.add(btn_none)
+    global monet
+    monet=current_monet(message.text)
+    bot.send_message(message.chat.id, 'Какое количество запросов вы хотите добавить в график?',reply_markup=markup2)
+    bot.register_next_step_handler(message,is_default)
 
 def crut(message):
     global user
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+    markup1.add(btn1, btn2, btn3, btn4, btn5)
     if user.check_user()=='1':
-        bot.send_message(message.chat.id, 'Work')
+        bot.send_message(message.chat.id, 'Work',reply_markup=markup1)
     else:
-        bot.send_message(message.chat.id, 'Зарегистриуйтесь, введя апи кей и секрет кей через двоеточие')
+        bot.send_message(message.chat.id, 'Зарегистрируйтесь, введя апи кей и секрет кей через двоеточие')
         bot.register_next_step_handler(message, checker)
 
 
 def checker(message):
     global user
     user.init_api(message.text.split(':'))
+    bot.send_message('Вы успешно зарегистрировались!')
+
+def is_default(message):
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
+    markup1.add(btn1, btn2, btn3, btn4, btn5)
+    get_message_bot = message.text.strip().lower()
+    global monet
+    print(monet.name)
+    if(get_message_bot=='по умолчанию'):
+        try:
+            token = binance_script.Start_interface(message.chat.id, monet.name)
+            info = token.get_volume(2, 200)
+            bot.send_message(message.chat.id, f'Объём продаж: {info[0]}\nОбъём покупок: {info[1]}\n', parse_mode='html', reply_markup=markup1)
+        except BaseException:
+            bot.send_message(message.chat.id, "Вы ввели неправильное название монеты")
+    else:
+        volume_default(message)
 
 bot.polling(none_stop=True)
